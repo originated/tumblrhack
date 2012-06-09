46f9f7b788db3929c8f6b5d9bfd6d2c0976d86dc
Cifani <mcifani@gmail.com>
2012-06-09 16:51:23 -0700

    all

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xhack::Application.config.secret_token = 'e6edb82e7452ad6e14852d76086f08b01a7e3e7dfbc2813fd0668e43a6d4825a3210570a917ba9b13ba5a1fa63f81de9ada9568e914eafc032a9b26cfa786d6a'
